4fd664f93e8777a7ae058b5421643da3b12e9568
Adam Shilling <admshng@gmail.com>
2020-09-10 19:21:02 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,7 +3,6 @@ smallest_numbers = []
 row_index = 0
   while row_index < src.count do 
     element_index = 0 
-    smallest_number = ""
     while element_index < src[row_index].count do 
       if src[row_index][element_index] <= src[row_index][0] 
         smallest_number = src[row_index][element_index]
